19a3a8e028f5ac507ba385096125f7560fbbf18e
LuisEduardoC <lecalderoncanto@gmail.com>
2020-01-18 15:56:07 -0500

    listas y vectores

---
+++ listas y vectores.R
@@ -0,0 +1,10 @@
+numero=list(1,2,3)
+numero
+numero=c(1,2,3)
+numero
+
+#se pone comillar
+x=c('a','b','c','d')
+x1=c(20,32,53,56)
+x2=c('hombre','mujer','hombre','mujer')
+dataframe=data.frame(casos=x,edad=x1,sexo=x2) 
