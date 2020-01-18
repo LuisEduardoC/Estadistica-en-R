e8bac44b3028ee707d802016be55766ca8ea027d
LuisEduardoC <lecalderoncanto@gmail.com>
2020-01-18 16:58:00 -0500

    temas adicionales

--- listas y vectores.R
+++ listas y vectores.R
@@ -8,3 +8,16 @@ x=c('a','b','c','d')
 x1=c(20,32,53,56)
 x2=c('hombre','mujer','hombre','mujer')
 dataframe=data.frame(casos=x,edad=x1,sexo=x2) 
+dataframe
+#importar datos
+datos
+testData = read.spss("C:/Users/ADMIN/Dropbox/BookData/chapter4/Employee Data.sav",to.data.frame = T)
+install.packages("rio")
+library(rio)
+head(datos)
+read.spss
+library(foreign)
+head(testDatos)
+summary(testData)
+
+
