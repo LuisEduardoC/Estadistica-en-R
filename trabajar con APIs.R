f8f2892796899930e69f04b020750496ff36f61c
LuisEduardoC <lecalderoncanto@gmail.com>
2020-01-21 15:03:04 -0500

    se ejecuta a traves del formato data.pjson

--- trabajar con APIs.R
+++ trabajar con APIs.R
@@ -16,9 +16,15 @@ FORMATO='data.json/'
 
 #El GUID se encuentrta disponible en la pagina, sólo queda agregar el key
 
-GUID='http://api.datosabiertos.msi.gob.pe/api/v2/dashboards/FISCA.json/'
+GUID='http://api.datosabiertos.msi.gob.pe/api/v2/datastreams/INTER-DE-SEREN-2019/'
 FORMATO='data.json/'
-millave='db80a7dbc0a6b9f1fbdef548783938cc7b00d4e3'
+millave='6242186595f840debd6cf1730633a0230e5d7d5e'
 request=paste0(GUID,FORMATO,'?auth_key=',millave)
 #verificar la dirección
 request
+serenosSI = fromJSON(request)
+serenosSI
+FORMATO='data.pjson/'
+request2=paste0(GUID,FORMATO,'?auth_key=',millave)
+serenosSI = fromJSON(request2)
+serenosSI 
